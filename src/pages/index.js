8be50638b11ed3cb76b6c3814e458fd1aba24ef3
Egor Talovikov <egtalovikov@gmail.com>
2022-10-16 17:54:44 +0300

    feat: сделал мобильную верстку блока с видео, подключил и реализовал Slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,18 @@
 import '../pages/index.css';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+new Swiper('.video-slider', {
+  spaceBetween: 30,
+  breakpoints: {
+    414: {
+      slidesPerView: 1.35,
+    },
+    768: {
+      slidesPerView: 2.70,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  },
+})
