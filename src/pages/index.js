daf72fdf763b3d572eb6431f421438f912194996
Egor Talovikov <egtalovikov@gmail.com>
2022-10-17 09:34:32 +0300

    feat: сделал десктоп версию блока, доделал Swiper

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,19 @@ import '../pages/index.css';
 import Swiper from 'swiper';
 import 'swiper/css';
 
-new Swiper('.video-slider', {
+new Swiper('.two-video-slider', {
+  spaceBetween: 30,
+  breakpoints: {
+    414: {
+      slidesPerView: 1.35,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+})
+
+new Swiper('.three-video-slider', {
   spaceBetween: 30,
   breakpoints: {
     414: {
