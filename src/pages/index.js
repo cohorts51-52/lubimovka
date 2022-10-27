0026da4c33036ad9e89d4fd8f5f2636d602574bb
Yauheni5 <yauheni5@github.com>
2022-10-27 17:26:41 +0300

    fix: slider PerView

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,24 +29,7 @@ new Swiper('.three-video-slider', {
   },
 })
 
-const personSwiper = new Swiper('.persons-swiper', {
-  breakpoints: {
-    414: {
-      slidesPerView: 1.5,
-    },
-    500: {
-      slidesPerView: 2.0,
-    },
-    700: {
-      slidesPerView: 2.25,
-    },
-    900: {
-      slidesPerView:3,
-    },
-    1024: {
-      slidesPerView: 4,
-    }
-  },
+new Swiper('.persons-swiper', {
+  spaceBetween: 30,
+  slidesPerView:'auto'
 })
-
-personSwiper.updateSize()
